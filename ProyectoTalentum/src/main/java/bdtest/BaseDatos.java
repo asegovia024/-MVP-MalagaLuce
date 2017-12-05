616b9a4642c4ebd3d93e139d70ba0feb1b846cb0
charly6596 <cgutisanc@gmail.com>
2017-12-05 14:14:26 +0100

    objeto de conexion y ejecucion de consultas listo; TODO: helper para acceder a sus datos de la bd

--- ProyectoTalentum/src/main/java/bdtest/BaseDatos.java
+++ ProyectoTalentum/src/main/java/bdtest/BaseDatos.java
@@ -2,41 +2,40 @@ package bdtest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
  
 /**
  *
  * @author sqlitetutorial.net
  */
 public class BaseDatos {
-     /**
-     * Connect to a sample database
-     */
+	
+	//Esta variable constante almacena la ruta de la base de datos SQLite
 	private final String URL_DEFAULT = "jdbc:sqlite:C:/workspace/db/proyecto.s3db";
 	Connection conn = null;
     /**
-     * Realiza la conexión a la base de datos
+     * Realiza la conexión a la base de datos SQLite, 
+     * @return Devuelve la conexión de tipo Connection 
      */
     private Connection connect() {
         try {            
             // create a connection to the database
+        	
             conn = DriverManager.getConnection(URL_DEFAULT);
             System.out.println("Se ha conectado a la base de datos SQLite.");
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
         return conn;
     }
-    public void close(){
+    
+    /**
+     * Cierra la conexión de la base de datos
+     */
+    public void closeConnection(){
     	try {
 			conn.close();
             System.out.println("Se ha desconectado a la base de datos SQLite.");
@@ -45,12 +44,26 @@ public class BaseDatos {
 		}
     }
     /**
-     * @param args the command line arguments
+     * @param query - Parámetro de tipo String, que contendrá el valor de la query a ejecutar
+     * @return Devuelve un parámetro tipo ResulSet con el resultado de la query
      */
-    public static void main(String[] args) {
+    public ResultSet Query(String query){
+    	ResultSet rs = null;
+    	try {
+    		Connection conn = this.connect();
+            Statement stmt  = conn.createStatement();
+            rs              = stmt.executeQuery(query);
+           
+           } catch (SQLException e) {
+               System.out.println(e.getMessage());
+           }
+    	return rs;
+    }
+    /**
+     * Prueba de conexión
+     */
+    public static void main(String[] args) throws SQLException {
     	BaseDatos db = new BaseDatos();
-    	db.connect();
-    	db.connect();
-    	db.close();
+    	ResultSet rs = db.Query("SELECT * FROM usuario");
     }
 }
