bfe0e7fb8a2eec17eb13190b165a94cf83d293be
antonio martin <antonio_296@hotmail.es>
2017-12-05 14:51:14 +0100

    Terminando metodos superclase

--- ProyectoTalentum/src/main/java/ProyectoTalentum/ProyectoTalentum/SuperUsuario.java
+++ ProyectoTalentum/src/main/java/ProyectoTalentum/ProyectoTalentum/SuperUsuario.java
@@ -2,7 +2,7 @@ package ProyectoTalentum.ProyectoTalentum;
 import ProyectoTalentum.ProyectoTalentum.Usuario;
 import ProyectoTalentum.ProyectoTalentum.Accion;
 
-public class SuperUsuario extends Usuario {
+public class SuperUsuario extends Usuario{
 
 	
 	public SuperUsuario(String nombre, String correo, String pass, String fecha) {
@@ -13,10 +13,38 @@ public class SuperUsuario extends Usuario {
 		
 	}
 	
-	public  boolean Validar(){
-		
+	//Acciones
+	/**
+	 * @param accionAvalidar Objeto a validar
+	 * @return devuelve true en caso de que se haya realizado correctamente
+	 */
+	public  boolean Validar(Accion accionAvalidar){
+		accionAvalidar.Validar(); //cambia el estado de una accion a validado
 		
 		return true;
+	}
+	
+	
+	/**
+	 * @param nombre Se Introduce el nombre del equipo
+	 * @param color Introduce el color en formato hexadecimal
+	 * @param escudo Introduce la ruta de la imagen
+	 * @return devuelve true en caso de que se haya realizado correctamente
+	 */
+	public  boolean CrearEquipo(String nombre, String color, String escudo){	
+		Equipo equipo = new Equipo(nombre, color, escudo); //este equipo hay que guardarlo juego en la BBDD
+		return true;
+	}
+	
+	
+	public  boolean AddUsuario(Usuario usuario, Equipo equipo){
+		
+		if(usuario.getTeam() == null){
+			usuario.setTeam(equipo);//si el usuario no tiene equipo se añade al designado
+			return true;
+		}
+		
+		return false;
 	}
 	
 
