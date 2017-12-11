caeb89305675b6e3b05f3a6a4cff5c5de7770626
AntonioMartinGamez <antonio_296@hotmail.es>
2017-12-11 09:03:22 +0100

    Añadiendo imagen en jpanel

--- ProyectoTalentum/src/main/java/Vistas/MpPanel.java
+++ ProyectoTalentum/src/main/java/Vistas/MpPanel.java
@@ -1,10 +1,29 @@
 package Vistas;
 
 
-
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 public class MpPanel {
-	
+	extends javax.swing.JPanel {
+	    int x, y;
+
+	    public Imagen(JPanel jPanel1) {
+	        this.x = jPanel1.getWidth();
+	        this.y = jPanel1.getHeight();
+	        this.setSize(x, y);
+	    }
+
+	    @Override
+	    public void paint(Graphics g) {
+	        ImageIcon Img = new ImageIcon(getClass().getResource("/Images/imagen1.png"));
+	        g.drawImage(Img.getImage(), 0, 0, x, y, null);
+	    }    
+
+	}
+
+
 
 
 }
